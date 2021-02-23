3709c278285f1819fc96859d795810d36ee45bb1
WaledSalem <eng.walied.salem.capw@gmail.com>
2021-02-23 21:17:08 +0000

    service-1 passed test

---
+++ service-1/tests/test_unit.py
@@ -0,0 +1,18 @@
+from unittest.mock import patch
+from flask import url_for
+from flask_testing import TestCase
+
+from app import app
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+class TestResponse(TestBase):
+    def test_shooter(self):
+    # We will mock request.get and request.post
+        with patch("requests.get") as g:
+            url = 'http://localhost:5001/shooter'
+            g.return_value.text = "Left"
+            response = self.client.get(url_for("index"))
+            self.assertIn(b'The Shooter direction is Left', response.data)
---
+++ service-4/tests/test_unit.py
@@ -0,0 +1,5 @@
+from flask import url_for
+from flask_testing import TestCase
+
+from app import app
+
