e1f74146ddcc4fc03c7a0b17219b9408bf97e6b3
WaledSalem <eng.walied.salem.capw@gmail.com>
2021-02-24 00:52:34 +0000

    service 4 pass 79%

--- service-4/tests/test_unit.py
+++ service-4/tests/test_unit.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 from flask import url_for
 from flask_testing import TestCase
 
@@ -6,3 +7,31 @@ from app import app
 class TestBase(TestCase):
     def create_app(self):
         return app
+
+class TestResponse(TestBase):
+    def test_chance_left(self):
+        with patch("requests.get") as g:
+            url = 'http://localhost:5001/shooter'
+            url = 'http://localhost:5002/goalie'
+            g.return_value.text = "Left"               
+            
+            response = self.client.get(url_for("chance"))
+            self.assertIn(b'63%', response.data)
+
+    def test_chance_middle(self):
+        with patch("requests.get") as g:
+            url = 'http://localhost:5001/shooter'
+            url = 'http://localhost:5002/goalie'
+            g.return_value.text = "Middle"               
+            
+            response = self.client.get(url_for("chance"))
+            self.assertIn(b'0%', response.data)
+
+    def test_chance_right(self):
+        with patch("requests.get") as g:
+            url = 'http://localhost:5001/shooter'
+            url = 'http://localhost:5002/goalie'
+            g.return_value.text = "Right"               
+            
+            response = self.client.get(url_for("chance"))
+            self.assertIn(b'44%', response.data)
