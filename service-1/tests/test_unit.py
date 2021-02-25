fc93731b7cb7601d8f3d012eefca407c0a2936c1
WaledSalem <eng.walied.salem.capw@gmail.com>
2021-02-25 01:37:17 +0000

    test complete

--- service-1/tests/test_unit.py
+++ service-1/tests/test_unit.py
@@ -8,6 +8,11 @@ class TestBase(TestCase):
     def create_app(self):
         return app
 
+class TestHome(TestBase):
+    def test_home_get(self):
+        response = self.client.get(url_for('home'))
+        self.assertEqual(response.status_code, 200)
+
 class TestResponse(TestBase):
     def test_shooter(self):
     # We will mock request.get and request.post
@@ -16,3 +21,8 @@ class TestResponse(TestBase):
             g.return_value.text = "Left"
             response = self.client.get(url_for("index"))
             self.assertIn(b'The Shooter direction is Left', response.data)
+
+class TestRead(TestBase):
+    def test_Read_get(self):
+        response = self.client.get(url_for('read'))
+        self.assertEqual(response.status_code, 200)
