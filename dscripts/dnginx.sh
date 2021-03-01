4e4f3dee0b29a2bf5202292ddd9326c47de242a5
WaledSalem <eng.walied.salem.capw@gmail.com>
2021-03-01 04:15:53 +0000

    prune

--- dscripts/dnginx.sh
+++ dscripts/dnginx.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 ssh -i ~/.ssh/jenkins_agent_key swanginx << EOF
 
-
-cd kick-off/loadbalancer/
-
-docker-compose down --rmi all 
-
+docker system prune --all
 sudo rm -rf kick-off
 
 EOF
