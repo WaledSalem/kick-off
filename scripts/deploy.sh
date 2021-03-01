01c48708b3ec49f72299837c65bd6dd60429ee48
WaledSalem <eng.walied.salem.capw@gmail.com>
2021-03-01 01:52:56 +0000

    deploy 2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,6 @@ git clone https://github.com/WaledSalem/kick-off.git
 
 cd kick-off
 
-sudo docker stack rm kick-stack
+docker stack rm kick-stack
 
-sudo docker stack deploy --compose-file docker-compose.yaml kick-stack
+docker stack deploy --compose-file docker-compose.yaml kick-stack
