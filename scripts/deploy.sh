61a883820713d83533663ae69c66077963c25b00
WaledSalem <eng.walied.salem.capw@gmail.com>
2021-03-01 02:40:26 +0000

    nginx

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,3 +11,16 @@ docker stack rm kick-stack
 
 docker stack deploy --compose-file docker-compose.yaml kick-stack
 EOF
+
+ssh -i ~/.ssh/jenkins_agent_key swanginx << EOF
+
+sudo rm -rf kick-off
+
+git clone https://github.com/WaledSalem/kick-off.git
+
+cd kick-off/loadbalancer/
+
+docker rm -f swanginx
+
+docker run -d -p 80:80 --name swanginx --mount type=bind,source=./nginx/nginx.conf,target=/etc/nginx/nginx.conf nginx:alpine
+EOF
