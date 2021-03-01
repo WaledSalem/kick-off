3e35b5daeabd6695d5bec5db3b1ab31ba14ee685
WaledSalem <eng.walied.salem.capw@gmail.com>
2021-03-01 01:47:59 +0000

    deploy try1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-pwd
+ssh -i ~/.ssh/jenkins_agent_key swarmanager
+
+sudo rm -rf kick-off
+
+git clone https://github.com/WaledSalem/kick-off.git
+
+cd kick-off
+
+sudo docker stack rm kick-stack
+
+sudo docker stack deploy --compose-file docker-compose.yaml kick-stack
